1040afbf06accc80cf1cc8946c55de458a8d80df
DevNadimm <nadimchowdhury87@gmail.com>
2025-03-09 00:11:43 +0600

    initial project

---
+++ lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:new_app/core/services/localization_service.dart';
+import 'package:new_app/core/themes/theme.dart';
+import 'package:new_app/features/medication/screens/add_medications_screen.dart';
+
+class MyApp extends StatelessWidget {
+  final Locale locale;
+
+  const MyApp({super.key, required this.locale});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'MY RX',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      theme: appTheme,
+      locale: locale,
+      fallbackLocale: LocalizationService.defaultLocale,
+      translations: LocalizationService(),
+      home: const AddMedicationsScreen(),
+    );
+  }
+}
