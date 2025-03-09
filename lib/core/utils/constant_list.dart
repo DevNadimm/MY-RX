9e13541fa48eff567e79aec49f717f6a013891de
DevNadimm <nadimchowdhury87@gmail.com>
2025-03-09 11:51:23 +0600

    update

---
+++ lib/core/utils/constant_list.dart
@@ -0,0 +1,5 @@
+class ConstantList {
+  ConstantList._();
+
+  static List<String> drugList = ["Tablet", "Capsule", "Syrup", "Injection", "Ointment", "Drops", "Powder", "Gel", "Cream", "Spray", "Vaccine"];
+}
--- lib/features/medication/screens/add_medication_screen.dart
+++ lib/features/medication/screens/add_medication_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_utils/src/extensions/export.dart';
+import 'package:new_app/core/utils/constant_list.dart';
 import 'package:new_app/shared/widgets/app_bar_bottom_divider.dart';
 import 'package:new_app/shared/widgets/custom_bottom_sheet.dart';
 
@@ -11,33 +12,8 @@ class AddMedicationScreen extends StatefulWidget {
 }
 
 class _AddMedicationScreenState extends State<AddMedicationScreen> {
-  TextEditingController medicationName = TextEditingController();
+  final TextEditingController medicationName = TextEditingController();
   final TextEditingController drugType = TextEditingController();
-  List<String> drugList = [
-    "Inhaled",
-    "Tablet",
-    "Capsule",
-    "Syrup",
-    "Injection",
-    "Ointment",
-    "Drops",
-    "Powder",
-    "Suppository",
-    "Patch",
-    "Gel",
-    "Lozenge",
-    "Nasal Spray",
-    "Ear Drops",
-    "Eye Drops",
-    "Chewable Tablet",
-    "Liquid",
-    "Cream",
-    "Solution",
-    "Suspension",
-    "Pill",
-    "Vaccine",
-    "Spray"
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -55,36 +31,36 @@ class _AddMedicationScreenState extends State<AddMedicationScreen> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "Medication description or nickname",
+              'medication_name_label'.tr,
               style: Theme.of(context).textTheme.titleMedium,
             ),
             Padding(
               padding: const EdgeInsets.only(top: 10),
               child: TextFormField(
                 controller: medicationName,
-                decoration: const InputDecoration(
-                  hintText: "Type here...",
+                decoration: InputDecoration(
+                  hintText: 'type_here'.tr,
                 ),
               ),
             ),
             const SizedBox(height: 20),
             Text(
-              "Drug Type",
+              'drug_type'.tr,
               style: Theme.of(context).textTheme.titleMedium,
             ),
             Padding(
               padding: const EdgeInsets.only(top: 10),
               child: TextFormField(
                 controller: drugType,
-                decoration: const InputDecoration(
-                  hintText: "Select",
+                decoration: InputDecoration(
+                  hintText: 'select'.tr,
                 ),
                 readOnly: true,
                 onTap: () {
                   showCustomBottomSheet(
-                      items: drugList,
+                      items: ConstantList.drugList,
                       controller: drugType,
-                      title: 'Select Drug',
+                      title: 'select_drug'.tr,
                   );
                 },
               ),
