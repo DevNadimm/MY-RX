2d7ad2258fdcb2f667f0ae6b4d422636f59f0ef0
DevNadimm <nadimchowdhury87@gmail.com>
2025-05-09 21:04:05 +0600

    update

---
+++ lib/core/temp/medication_list.dart
@@ -0,0 +1,20 @@
+import 'package:new_app/features/medication/models/medication_model.dart';
+
+final List<MedicationModel> medications = [
+  MedicationModel(
+    name: 'Paracetamol',
+    type: 'Tablet',
+    startDate: '2025-05-01',
+    endDate: '2025-05-07',
+    time: '08:00 AM',
+    timing: 'Before Eat',
+  ),
+  MedicationModel(
+    name: 'Ibuprofen',
+    type: 'Capsule',
+    startDate: '2025-05-02',
+    endDate: '2025-05-10',
+    time: '06:00 PM',
+    timing: 'After Eat',
+  ),
+];
---
+++ lib/features/medication/models/medication_model.dart
@@ -0,0 +1,17 @@
+class MedicationModel {
+  final String name;
+  final String type;
+  final String startDate;
+  final String endDate;
+  final String time;
+  final String timing;
+
+  MedicationModel({
+    required this.name,
+    required this.type,
+    required this.startDate,
+    required this.endDate,
+    required this.time,
+    required this.timing,
+  });
+}
