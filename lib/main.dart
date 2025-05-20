a5bbc79e90c8923c81d5ddd74c0fb2fa206c7ad3
DevNadimm <nadimchowdhury87@gmail.com>
2025-05-20 19:32:41 +0600

    update - pubspec, android manifest, main func

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:new_app/app.dart';
 import 'package:new_app/core/services/localization_service.dart';
+import 'package:new_app/features/medication/services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await NotificationService().init();
 
   Locale locale = await LocalizationService.getSavedLocale();
   await LocalizationService.loadTranslations();
